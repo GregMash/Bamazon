a5446505dcf7fed798dfb32bd4dcf17ea014ddbc
Link <username@users.noreply.github.com>
2019-01-25 23:20:42 -0700

    commented

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,4 @@
+//============================================ GLOBAL VARIABLES ============================================
 const mysql = require('mysql');
 const dotenv = require('dotenv').config();
 const keys = require('./keys');
@@ -15,10 +16,9 @@ let quantityBought = [];
 let priceArray = [];
 let productNameArr = [];
 
+//============================================ FUNCTIONS ============================================
 
-connection.connect(() => {
-});
-
+// This function welcomes the user and prompts them to shop or leave
 function welcome() {
     console.clear();
     console.log(`
@@ -38,35 +38,32 @@ Welcome to the Bamazon online shop!`);
     })
 };
 
-
+// This function displays the products available for sale 
 function displayItems() {
     const query = 'SELECT product_id, product_name, department_name, price FROM products';
     connection.query(query, (err, res) => {
         console.log('The items available for sale are:');
         for (let i = 0; i < res.length; i++) {
             currentSaleItems.push(res[i]);
-            prettyLog(res[i]);
+            console.log(`
+Product ID#: ${res[i].product_id}
+Product Name: ${res[i].product_name}
+Department Name: ${res[i].department_name}
+Price: $${res[i].price}
+---------------------------------------------------`);
         };
         promptUser();
     })
 };
 
-
+// End the connection to the database
 function endConnection() {
     connection.end(function (err) {
         if (err) throw err;
     })
 };
 
-function prettyLog(data) {
-    console.log(`
-Product ID#: ${data.product_id}
-Product Name: ${data.product_name}
-Department Name: ${data.department_name}
-Price: $${data.price}
----------------------------------------------------`)
-};
-
+// This function will prompt the user to pick an item to purchase from the displayed list
 function promptUser() {
     inquirer.prompt({
         name: 'buy',
@@ -78,11 +75,11 @@ function promptUser() {
             promptUser();
         } else {
             displayItemPicked(res.buy);
-        }
-    }
-    )
+        };
+    })
 };
 
+// This function displays the information for the item that the user picks to purchase
 function displayItemPicked(item) {
     const query = `SELECT product_name, department_name, price FROM products WHERE product_id = ${item}`
     connection.query(query, function (err, res) {
@@ -98,10 +95,10 @@ ${res[0].department_name}
 $${res[0].price}`);
             confirmCart(item);
         };
-
     })
 };
 
+// this will prompt the user to confirm their selection or return to the products display page
 function confirmCart(item) {
     inquirer.prompt({
         name: 'confirm',
@@ -118,6 +115,7 @@ function confirmCart(item) {
     })
 };
 
+// this function handles the quantity of the item the user wishes to buy, checking the stock level, and updating the inventory level
 function addQuantity() {
     inquirer.prompt({
         name: 'quantity',
@@ -134,23 +132,22 @@ function addQuantity() {
                 connection.query(query, (err, result) => {
                     let quantity = result[0].stock_quantity;
                     if (quantity - res.quantity >= 0) {
-                        connection.query(updateQuery, (err, response) =>{
-                            console.log(response);
-                            console.log(`Items are in stock! ${res.quantity} added to cart!`);
-                            quantityBought.push(res.quantity);
-                            shopOrPay();
+                        console.log(`Items are in stock! ${res.quantity} added to cart!`);
+                        quantityBought.push(res.quantity);
+                        connection.query(updateQuery, (err, response) => {
                         })
+                        shopOrPay();
                     } else {
                         console.log(`Sorry, the store only has ${quantity} left in stock. Please enter a different quantity`);
                         addQuantity();
-                    }
+                    };
                 })
             };
-           
-        }
+        };
     })
 };
 
+// This function will prompt about continuing to shop or checkout and add the product they select to their cart
 function shopOrPay() {
     inquirer.prompt({
         name: 'confirm',
@@ -174,6 +171,7 @@ function shopOrPay() {
     })
 };
 
+// This function will calculate the price based on item selected and quantity selected and give the user their total
 function checkout(priceArray, productNameArr) {
     let finalPrice = 0;
     console.clear();
@@ -188,5 +186,6 @@ ${quantityBought[i]} ${productNameArr[i]} * $${priceArray[i]} = $${price}`);
     Thanks for shopping at Bamazon!`);
 };
 
-
+//============================================ MAIN PROCESS ============================================
+//call the first function
 welcome();
